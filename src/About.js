1e311a14ac90849bbcc910459023462876e6ff25
Sam <mcmyler@gmail.com>
2017-06-04 17:01:30 -0500

    Update skills/intersts

--- src/About.js
+++ src/About.js
@@ -1,28 +1,24 @@
 import React from 'react'
 
 export const About = () => {
+  const skills = ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js/Express']
+  const interests = ['Bootstrap', 'D3', 'Elixir']
   return (
-    <div className="App-about-container">
-      <div className="App-about center">
+    <div className='App-about-container'>
+      <div className='App-about center'>
         <h2>About</h2>
         <p>Hi! My name is Sam. I develop things.</p>
-        <div className="lists">
+        <div className='lists'>
           <div>
             <p>Skills:</p>
             <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>JavaScript</li>
-              <li>Bootstrap</li>
+              { skills.map((skill) => <li>{skill}</li>)}
             </ul>
           </div>
           <div>
             <p>Interests:</p>
             <ul>
-              <li>React</li>
-              <li>Node.js/Express</li>
-              <li>D3</li>
-              <li>Elixir</li>
+              { interests.map((interest) => <li>{interest}</li>)}
             </ul>
           </div>
         </div>
