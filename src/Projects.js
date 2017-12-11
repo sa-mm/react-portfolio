6d7ac0d2f26324e2243e19832526273891ddb351
Sam <mcmyler@gmail.com>
2017-12-11 00:02:26 -0600

    Standard style

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 import { ProjectsSlider } from './Carousel'
 
 export const Projects = () => (
-  <div className="App-portfolio-container">
-  <div className="App-portfolio">
-    <h2>Portfolio</h2>
-    <ProjectsSlider />
-  </div>
+  <div className='App-portfolio-container'>
+    <div className='App-portfolio'>
+      <h2>Portfolio</h2>
+      <ProjectsSlider />
+    </div>
   </div>
 )
