1c7c8ab3dfbc807b031495657ffe57f4833c3080
Sam <mcmyler@gmail.com>
2018-01-16 15:01:56 -0600

    Add linkedin

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,7 +29,7 @@ export const About = () => {
       </div>
       <div className='About-contact'>
         <h2 className='heading'>Contact</h2>
-        <p>I'm <em>sa-mm</em> on <a href='https://github.com/sa-mm' target='_blank' rel='noopener noreferrer' style={color}>GitHub <i className='fa fa-github' /></a>. You can also contact me <a href='https://twitter.com/s_pugly' target='_blank' rel='noopener noreferrer' style={color}>@s_pugly <i className='fa fa-twitter' /></a> on Twitter.</p>
+        <p>I'm <em>sa-mm</em> on <a href='https://github.com/sa-mm' target='_blank' rel='noopener noreferrer' style={color}>GitHub</a> <i className='fa fa-github' />. You can contact me <a href='https://twitter.com/s_pugly' target='_blank' rel='noopener noreferrer' style={color}>@s_pugly</a> <i className='fa fa-twitter' /> on Twitter or get in touch via <a href='https://www.linkedin.com/in/sam-mcmyler/' target='_blank' rel='noopener noreferrer' style={color}>LinkedIn</a> <i className="fa fa-linkedin" aria-hidden="true" />.</p>
       </div>
     </div>
   )
