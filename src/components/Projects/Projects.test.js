5beda84ce6d8ae90dae65dac7369f3694a005525
Sam <mcmyler@gmail.com>
2018-01-17 14:09:46 -0600

    Fix for failing test

--- src/components/Projects/Projects.test.js
+++ src/components/Projects/Projects.test.js
@@ -2,7 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Projects } from './Projects'
 
+const props = {
+  handleProjectsMount: jest.fn(),
+  hasMountedProjects: false
+}
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Projects />, div)
+  ReactDOM.render(<Projects {...props} />, div)
 })
